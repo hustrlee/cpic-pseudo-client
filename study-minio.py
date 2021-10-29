87cab339f947ba59efbb4c8a0c3d33cf4998e1ce
Ryan Li <rlee@163.com>
2021-10-29 22:35:46 +0800

    -

--- study-minio.py
+++ study-minio.py
@@ -4,7 +4,25 @@ from minio.error import S3Error
 
 def main():
     client = Minio(
-        "http://localhost:9000",
+        "localhost:9000",
         access_key="cpic-gd",
-        secret_key="cpic-gd-123"
+        secret_key="cpic-gd-123",
+        secure=False
     )
+    bucket_name = "cpic"
+
+    if not client.bucket_exists(bucket_name=bucket_name):
+        client.make_bucket(bucket_name=bucket_name)
+
+    client.fput_object(
+        bucket_name,
+        "1.jpeg",
+        "gd-medical-bill-sample.jpg"
+    )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error ocurred.", exc)
