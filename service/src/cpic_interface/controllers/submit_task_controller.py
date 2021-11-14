4e0a49aaaba431ea09a2dc1037f54a3e97c906da
Ryan Li <hustrlee@gmail.com>
2021-11-14 21:14:10 +0800

    -

---
+++ service/src/cpic_interface/controllers/submit_task_controller.py
@@ -0,0 +1,21 @@
+import connexion
+import six
+
+from cpic_interface.models.create_case_dto import CreateCaseDto  # noqa: E501
+from cpic_interface.models.create_case_res_dto import CreateCaseResDto  # noqa: E501
+from cpic_interface import util
+
+
+def create_case(create_case_dto=None):  # noqa: E501
+    """用于接收渠道推送的案件信息
+
+     # noqa: E501
+
+    :param create_case_dto: 
+    :type create_case_dto: dict | bytes
+
+    :rtype: CreateCaseResDto
+    """
+    if connexion.request.is_json:
+        create_case_dto = CreateCaseDto.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ service/src/cpic_interface/test/test_query_result_controller.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+import unittest
+
+from flask import json
+from six import BytesIO
+
+from cpic_interface.models.query_case_dto import QueryCaseDto  # noqa: E501
+from cpic_interface.models.query_case_res_dto import QueryCaseResDto  # noqa: E501
+from cpic_interface.test import BaseTestCase
+
+
+class TestQueryResultController(BaseTestCase):
+    """QueryResultController integration test stubs"""
+
+    def test_query_case(self):
+        """Test case for query_case
+
+        查询任务状态，及识别结果
+        """
+        query_case_dto = {
+  "custid" : "custid",
+  "uuid" : "uuid",
+  "token" : "token"
+}
+        headers = { 
+        }
+        response = self.client.open(
+            '/queryCase',
+            method='POST',
+            headers=headers,
+            data=json.dumps(query_case_dto),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    unittest.main()
