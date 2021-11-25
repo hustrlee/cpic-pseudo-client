d41e529878c8f180cb715642c301946c6bb5e005
Ryan Li <rlee@163.com>
2021-11-25 16:54:10 +0800

    -

--- worker-node/index.js
+++ worker-node/index.js
@@ -1,6 +1,20 @@
 const amqp = require("amqplib");
+const axios = require("axios");
+const Minio = require("minio");
+
+const minioClient = new Minio.Client({
+  endPoint: "localhost",
+  port: 9000,
+  useSSL: false,
+  accessKey: "minio",
+  secretKey: "minio123",
+});
 
 (async () => {
+  if (!minioClient.bucketExists("cases")) {
+    minioClient.makeBucket("cases");
+  }
+
   const connection = await amqp.connect("amqp://localhost");
   const channel = await connection.createChannel();
 
@@ -9,9 +23,36 @@ const amqp = require("amqplib");
 
   await channel.consume(
     "case_queue",
-    (msg) => {
+    async (msg) => {
+      // 获取案件信息
       let caseDetail = JSON.parse(msg.content.toString());
-      console.log(caseDetail.caseInfo.images);
+
+      // 将图片上传到 cases 桶
+      for (image of caseDetail.caseInfo.images) {
+        let imageStream = await axios({
+          method: "get",
+          url: image.url,
+          responseType: "stream",
+        });
+
+        await minioClient.putObject(
+          "cases",
+          `${caseDetail.caseNo}/${image.name}`,
+          imageStream.data
+        );
+
+        // 更新 URL
+        image.url = await minioClient.presignedUrl(
+          "GET",
+          "cases",
+          `${caseDetail.caseNo}/${image.name}`
+        );
+      }
+
+      // 启动 camunda process
+      console.log(caseDetail);
+
+      // 向 MQ 确认本消息已经处理完毕
       channel.ack(msg);
     },
     { noAck: false }
